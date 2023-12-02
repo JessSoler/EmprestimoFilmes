8550a8876fd8b81beed64091fdc856a28f6aec91
jessica <jessicasolersouza@outlook.com>
2023-12-02 16:55:50 -0300

    Add project files.

---
+++ EmprestimoLivros/Migrations/20231202184335_Inicial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmprestimoLivros.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LivroEmprestado",
+                table: "Emprestimos",
+                newName: "FilmeEmprestado");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FilmeEmprestado",
+                table: "Emprestimos",
+                newName: "LivroEmprestado");
+        }
+    }
+}
